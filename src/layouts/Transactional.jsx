fcbcffac9edd8867be1c9b80592f661751a0979b
raffyalbar30 <geminglol09@gmail.com>
2026-05-29 16:32:13 +0700

    Paginations UI & Logic

--- src/layouts/Transactional.jsx
+++ src/layouts/Transactional.jsx
@@ -98,7 +98,7 @@ export default function Transactional() {
 
      
  const handleNextPages = async () => {
-   if (Page.pages >= Page?.endPage) {
+   if (Page.pages === Page?.endPage) {
       return;
    }
 
