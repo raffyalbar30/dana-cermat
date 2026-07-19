ef9b76640adfab0b29690ffb70b6ae37a9512483
raffyalbar30 <geminglol09@gmail.com>
2026-07-19 21:29:45 +0700

    final form login implementasi react-hook-form

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -9,10 +9,9 @@ import { useForm, Controller } from 'react-hook-form'
 
 
 export default function Login() {
-  const [ email, setemail ] = useState("");
-  const [ password, setpassword ] = useState("");  
   const [ response, setresponse ] = useState(""); 
   const [ viewPassword, setviewPassword] = useState(false); 
+  const [ loader, setloader ] = useState(false);
 
 //  React navigations 
   const navigate = useNavigate();
@@ -23,6 +22,8 @@ export default function Login() {
 
   const formLogin = (data) => {
      console.log(data.email)
+     console.log(data.password)
+     setloader(true)
   }
   const HandleSumbit = async (e) => {
      try {
@@ -39,11 +40,11 @@ export default function Login() {
        setresponse("Maaf email atau password salah!!")  
      }
   }
+ 
 
-  const HandleViewPassword = () => {
-    setviewPassword(prev => !prev);
-
-  }
+  setTimeout(() => {
+    setloader(false)
+  }, 1000)
 
 
   return (
@@ -65,10 +66,10 @@ export default function Login() {
                                 control={control}
                                 defaultValue=""
                                 rules={{
-                                    required: "Email wajib diisi",
+                                    required: "Email tidak boleh kosong!",
                                     pattern: {
                                         value: /^[^\s@]+@gmail\.com$/,
-                                        message: "Email harus pakai domain @gmail.com",
+                                        message: "Email harus pakai @gmail.com",
                                     },
                                     }}
                                 render={({field}) => (
@@ -77,49 +78,76 @@ export default function Login() {
                                         Class={"mt-2"}
                                         type={"email"}
                                         ClassParrent={"flex items-center relative"}
-                                        ClassInput={`p-3 rounded-xl bg-slate-100 focus:outline-none w-full focus:ring-2 ${errors.email ? "focus:ring-red-500 ring-2 ring-red-500" : "focus:ring-blue-500"}`}
+                                        ClassInput={`p-3 rounded-xl bg-slate-100 focus:outline-none w-full focus:ring-2 ${errors.email ? "focus:ring-red-500 ring-2 ring-red-500 placeholder:text-red-500" : "focus:ring-blue-500"}`}
                                         value={field.value}
                                         onChange={field.onChange}/>
                                 )}
                             />
+                            { errors.email && ( 
+                                  <span className='text-sm text-red-500'>{errors.email.message}</span>
+                            )}
                         </div>
 
                         <div>
                         <Label Children={"Password"} ClassText={"text-left text-gray-600"} />
-                        <Inputs
-                            Children={"Masukan password"}
-                            Class={"mt-2"}
-                            ClassParrent={"w-full flex items-center relative"}
-                            ClassInput={
-                            "w-full p-3 rounded-xl bg-slate-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            }
-                            type={viewPassword === true ? "text" : "password"}
-                            onChange={(e) => {
-                                setpassword(e.target.value)
-                            }}
-                            hiddenPassword={ 
-                            <button className='absolute cursor-pointer right-1 mr-2' onClick={() => HandleViewPassword()}>
-                                {viewPassword === true ? ( <PiEyeThin className='text-2xl' /> ) 
-                                :( <PiEyeSlashThin className='text-2xl' />)}
-                            </button>}
+                        <Controller
+                            name="password"
+                            control={control}
+                            defaultValue=""
+                             rules={{
+                                    required: "Password tidak boleh kosong!",
+                                        minLength: {
+                                            value: 4,
+                                            message: "Password minimal 4 character",
+                                        },
+                                        maxLength: {
+                                            value: 18,
+                                            message: "Password maksimal 18 character",
+                                        },
+                                        pattern: {
+                                             value: /^(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>_\-+=]).+$/,
+                                             message: "Password harus mengandung angka dan simbol",
+                                        }
+                                    }}
+                             render={({ field }) => (
+                                   <Inputs
+                                        Children={"Masukan password"}
+                                        Class={"mt-2"}
+                                        ClassParrent={"w-full flex items-center relative"}
+                                        ClassInput={`w-full p-3 rounded-xl bg-slate-100 focus:outline-none ${errors.password ? "focus:ring-red-500 ring-2 ring-red-500 placeholder:text-red-500" : "focus:ring-blue-500"}`}
+                                        type={viewPassword === true ? "text" : "password"}
+                                        value={field.value}
+                                        onChange={field.onChange}
+                                        hiddenPassword={ 
+                                        <div className='absolute cursor-pointer right-1 mr-2' onClick={() => setviewPassword(prev => !prev)}>
+                                            {viewPassword === true ? ( <PiEyeThin className='text-2xl' /> ) 
+                                            :( <PiEyeSlashThin className='text-2xl' />)}
+                                        </div>}
+                                    />
+                             )}
                         />
+                          { errors.password && ( 
+                                  <span className='text-sm text-red-500'>{errors.password.message}</span>
+                            )}
                         </div>
-
-                        {response && ( <span className="text-sm text-red-500">{response}</span>)}
                         <div className="flex justify-between text-sm text-blue-600 mt-2">
                         <Link to={"/Register"}>
                         <span className="cursor-pointer">Belum punya akun?</span>
                         </Link>
                         <span className="cursor-pointer">Lupa password?</span>
                         </div>
-
                         <Buttons
                         Classparrent={"mt-6"}
                         Classchild={"w-full"}
                         Classbutton={`
-                            w-full bg-[#3F47F4] disabled:cursor-not-allowed cursor-pointer transition text-white py-3 rounded-xl text-lg font-semibold`
+                            w-full ${loader === true ? "bg-indigo-400" : "bg-[#3F47F4]"} disabled:cursor-not-allowed cursor-pointer transition text-white py-3 rounded-xl text-lg font-semibold`
                         }
-                        Title={"Login"} type={"sumbit"}/>
+                        Title={loader === true ? (
+                            <div className='flex justify-center items-center gap-x-2'> 
+                                <svg width={"24px"} fill="hsl(228, 97%, 42%)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z"><animateTransform attributeName="transform" type="rotate" dur="0.75s" values="0 12 12;360 12 12" repeatCount="indefinite"/></path></svg>
+                                <span className='text-white text-md'>Loading</span>
+                            </div>
+                        ) : "Login"} type={"sumbit"}/>
                     </div>
                 </form>
             </div>
